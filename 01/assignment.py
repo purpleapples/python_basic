fd51e2109ddc731b3e9eddc178dbd1cfa8077dcb
purpleapples <tjdvkf19@gmail.com>
2020-08-26 17:30:40 +0900

    first_python_basic

---
+++ 01/assignment.py
@@ -0,0 +1,36 @@
+# assignment
+
+a = 1
+
+# 다중 치환 가능
+e, f = 3.8, 8.3
+print(e,f)
+
+
+#dynamic typing
+a = 1
+print(type(a))
+b  = True
+print(type(b))
+
+# extended assignment
+a = 10
+a += 10
+print(a)
+
+
+
+#swap
+x = 10
+y = 20
+print('===== swap =====')
+print(x, y)
+#
+# code area
+#
+tmp = y
+y = x
+x = tmp
+
+print(x, y)
+
